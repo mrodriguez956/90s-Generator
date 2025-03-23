f75f300f29b954a80b4cb031c34dc63fe34e20e2
mrodriguez956 <apple.mrodriguez956@gmail.com>
2025-03-23 06:13:45 -0500

    thank god for ImageStroke

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -3,7 +3,9 @@ import { Canvas, FabricImage, Path, Shadow } from 'fabric'; // browser
 import { useEffect, useRef, useState } from 'react';
 import perkBackground from './assets/img/perkbg.png'; //why did I need to create images.d.ts for this?
 import iconGradient from './assets/img/gradient.png'; //why did I need to create images.d.ts for this?
-import * as ImageTracer from "imagetracerjs";
+import ImageStroke from 'image-stroke';
+
+import rotate from 'image-stroke/lib/method-rotate'
 
 
 interface CanvasProps {
@@ -30,7 +32,7 @@ export function MainCanvas({ imgUpload }: CanvasProps) {
   useEffect(() => {
     if (imgUpload && canvas) {
       addIcon(imgUpload, canvas); //refer to stored canvas
-      console.log("Uploaded File (Data URL):", imgUpload);
+      
     }
   }, [imgUpload]); //imgUpload trigger = only runs when imgUpload changes
 
@@ -49,26 +51,8 @@ function addIcon(icon: string, canvas: Canvas) {
   console.log("Uploaded File (iconImage):", icon);
   gradImage.src = iconGradient;
 
-  iconImage.onload = () => {
-  ImageTracer.imageToSVG(iconImage.src, (svg: string) => {
-     const svgImage = new Path(svg, {
-         left: 0,
-         top: 0,
-         width: 250,
-         height: 250,
-         selectable: false,
-         stroke: 'white',
-         strokeWidth: 1,
-         fill: 'transparent',
-     });
-     canvas.add(svgImage);
-     canvas.renderAll();
-   }, { ltres: 0, qtres: 0 });
-   
-  };
-  
 
-  /*iconImage.onload = () => {
+  iconImage.onload = () => {
     gradImage.onload = () => {
       const fabricImage = new FabricImage(gradImage, {
         clipPath: new FabricImage(iconImage, {
@@ -89,14 +73,41 @@ function addIcon(icon: string, canvas: Canvas) {
 
       });
 
-
-
-      canvas.add(fabricImage);
+      canvas.add(fabricImage); //adding icon to canvas with modifications (without stroke)
       canvas.renderAll();
+
+      const dataURL = fabricImage.toDataURL(); //convert icon to dataURL as ImageStroke library expects an HTML image element, not a Fabric canvas
+      console.log("Data URL:", dataURL); //sanity check
+
+      canvas.remove(fabricImage); //remove icon from canvas
+
+      const htmlImage = new Image();
+      htmlImage.src = dataURL; //convert dataURL to HTML image element
+
+      htmlImage.onload = () => { //wait for html image to be loaded, apply stroke
+        const imageStroke = new ImageStroke();
+        imageStroke.use(rotate);
+
+        const resultCanvas = imageStroke.make(htmlImage, {
+          thickness: 2,
+          color: 'black'
+        });
+
+        const fabricImage = new FabricImage(resultCanvas, {
+          left: 0,
+          top: 0,
+          width: 250,
+          height: 250,
+          selectable: false,
+        });
+
+        canvas.add(fabricImage);
+        canvas.renderAll(); //BINGO!!!!!!!!!!!!!!!!
+      };
 
     };
   };
-  */
+
 }
 
 
