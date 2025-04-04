a9e9609a6c19848a0abe7df1339265d6709fe6ea
mrodriguez956 <apple.mrodriguez956@gmail.com>
2025-04-03 22:46:44 -0500

    no more duplicates.

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,38 +1,36 @@
-import { IconDisplay } from './IconDisplay';
+import { IconDisplay } from "./IconDisplay";
 
-import { Canvas, FabricImage } from 'fabric'; // browser
+import { Canvas, FabricImage } from "fabric"; // browser
 
-import { useEffect, useRef, useState } from 'react';
-import perkBackground from './assets/img/perkbg.png'; //why did I need to create images.d.ts for this?
-import iconGradient from './assets/img/gradient.png'; //why did I need to create images.d.ts for this?
-import ImageStroke from 'image-stroke';
-import rotate from 'image-stroke/lib/method-rotate'
-
+import { useEffect, useRef, useState } from "react";
+import perkBackground from "./assets/img/perkbg.png"; //why did I need to create images.d.ts for this?
+import iconGradient from "./assets/img/gradient.png"; //why did I need to create images.d.ts for this?
+import ImageStroke from "image-stroke";
+import rotate from "image-stroke/lib/method-rotate";
 
 interface CanvasProps {
   imgUpload?: string;
   imgName?: string;
-  files?: { name: string, data: string }[];
+  files?: { name: string; data: string }[];
 }
-
-
-
-
-
 
 export function MainCanvas({ /*imgUpload, imgName,*/ files }: CanvasProps) {
   const canvasEl = useRef<HTMLCanvasElement>(null);
   const downloadEl = useRef<HTMLAnchorElement>(null);
   const [canvas, setCanvas] = useState<Canvas | null>(null); //needed so that the canvas persists after re-renders. Initially, image uploads were causing re-renders then there would be no canvas to add to
-  const [downloadURL, setDownloadURL] = useState('');
-  const [processedFiles, setProcessedFiles] = useState<{name: string, data: string}[]>([]);
-  const [canvasURLs, setCanvasURLs] = useState<{name: string, data: string, id: number}[]>([]);
-
+  const [downloadURL, setDownloadURL] = useState("");
+  const [processedFiles, setProcessedFiles] = useState<
+    { name: string; data: string }[]
+  >([]);
+  const [canvasURLs, setCanvasURLs] = useState<
+    { name: string; data: string; id: number }[]
+  >([]);
 
   console.log("From Uploader:", files);
 
   useEffect(() => {
-    if (canvasEl.current) { //wait until canvas element exists to run
+    if (canvasEl.current) {
+      //wait until canvas element exists to run
       const newCanvas = new Canvas(canvasEl.current);
       setBackground(newCanvas);
       setCanvas(newCanvas); //store newCanvas into setCanvas for persistence
@@ -52,74 +50,67 @@ export function MainCanvas({ /*imgUpload, imgName,*/ files }: CanvasProps) {
   function downloadCanvas() {
     if (downloadEl.current && downloadURL) {
       downloadEl.current.href = downloadURL;
-      downloadEl.current.download = files?.[0]?.name + '.png'; //we need to add multifile download functionality here
-
+      downloadEl.current.download = files?.[0]?.name + ".png"; //we need to add multifile download functionality here
     }
   }
 
   const handleAddNewURL = (name: string, data: string) => {
     console.log("Before update:", canvasURLs);
-    const updateCanvasURLs = [
-      ...canvasURLs,
-      {
-        id: canvasURLs.length + 1,
-        name: name,
-        data: data,
-      },
-    ];
-    setCanvasURLs(updateCanvasURLs);
-    console.log("Updated canvas URLs:", updateCanvasURLs);
+    setCanvasURLs((prev) => [...prev, { name, data, id: prev.length + 1 }]);
+    console.log("Updated canvas URLs:", canvasURLs);
   };
 
-
-  function addIcon(icon: string, name: string, canvas: Canvas, setDownloadURL: (url: string) => void, handleAddNewURL: (name: string, data: string) => void) {
+  function addIcon(
+    icon: string,
+    name: string,
+    canvas: Canvas,
+    setDownloadURL: (url: string) => void,
+    handleAddNewURL: (name: string, data: string) => void
+  ) {
     const iconImage = new Image();
     const gradImage = new Image();
-  
+
     iconImage.src = icon;
     //console.log("Uploaded File (iconImage):", icon);
     gradImage.src = iconGradient;
-  
-  
+
     iconImage.onload = () => {
       gradImage.onload = () => {
         const fabricImage = new FabricImage(gradImage, {
           clipPath: new FabricImage(iconImage, {
-  
             left: 0,
             top: 0,
             width: 250,
             height: 250,
-            originX: 'center',  // Center the icon horizontally
-            originY: 'center',
-  
+            originX: "center", // Center the icon horizontally
+            originY: "center",
           }),
           left: 0,
           top: 0,
           width: 250,
           height: 250,
           selectable: false,
-  
         });
         //clear canvas before adding new icon
-  
+
         const dataURL = fabricImage.toDataURL(); //convert icon to dataURL as ImageStroke library expects an HTML image element, not a Fabric canvas
         console.log("Data URL created"); //sanity check
-  
+
         canvas.remove(fabricImage); //remove icon from canvas
-  
+
         const htmlImage = new Image();
         htmlImage.src = dataURL; //convert dataURL to HTML image element
-  
-        htmlImage.onload = () => { //wait for html image to be loaded, apply stroke
+
+        htmlImage.onload = () => {
+          //wait for html image to be loaded, apply stroke
           const imageStroke = new ImageStroke();
           imageStroke.use(rotate);
-  
+
           const resultIcon = imageStroke.make(htmlImage, {
             thickness: 2,
-            color: 'black'
+            color: "black",
           });
-  
+
           const fabricImage = new FabricImage(resultIcon, {
             left: 0,
             top: 0,
@@ -127,44 +118,38 @@ export function MainCanvas({ /*imgUpload, imgName,*/ files }: CanvasProps) {
             height: 250,
             selectable: false,
           });
-  
+
           canvas.add(fabricImage);
           canvas.renderAll(); //BINGO!!!!!!!!!!!!!!!!
-  
+
           const canvasURL = canvas.toDataURL();
           setDownloadURL(canvasURL);
-          
-          console.log("Canvas URL created for", name)
-         
+
+          console.log("Canvas URL created for", name);
+
           handleAddNewURL(name, canvasURL);
-   
+
+          canvas.remove(fabricImage);
         };
-        
       };
     };
-  
   }
 
   return (
     <>
+      {console.log("FINAL URLS:", canvasURLs)}
       <canvas ref={canvasEl} width={250} height={250}></canvas>
-      <a ref={downloadEl} onClick={downloadCanvas}> Download </a>
-      <IconDisplay files={processedFiles} />
+      <a ref={downloadEl} onClick={downloadCanvas}>
+        {" "}
+        Download{" "}
+      </a>
+      <IconDisplay files={canvasURLs} />
     </>
   );
-
 }
 
-
-
-
-
-
-
-
-
-
-function setBackground(canvas: Canvas) { //potential icon background selection?
+function setBackground(canvas: Canvas) {
+  //potential icon background selection?
   const bgImage = new Image();
   bgImage.src = perkBackground;
 
@@ -178,8 +163,4 @@ function setBackground(canvas: Canvas) { //potential icon background selection?
     canvas.backgroundImage = fabricImage;
     canvas.renderAll();
   };
-
-
-
-
 }
--- src/Uploader.tsx
+++ src/Uploader.tsx
@@ -1,95 +1,86 @@
-import { useEffect, useRef, useState } from 'react';
-import { MainCanvas } from './Canvas';
-import { file } from 'bun';
-
+import { useEffect, useRef, useState } from "react";
+import { MainCanvas } from "./Canvas";
 
 export function FileHandler() {
-
-    const fileInputRef = useRef<HTMLInputElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
   /* const [uploadedFile, setUploadedFile] = useState<string>(); //must match the prop in Canvas
    const [uploadedName, setUploadedName] = useState<string>(); */
-   const [fileData, setFileData] = useState<{name: string, data: string}[]>([]); //must match the prop in Canvas
-
-   function uploadFiles(event: any){
+  const [fileData, setFileData] = useState<{ name: string; data: string }[]>(
+    []
+  ); //must match the prop in Canvas
+  
+  const [isProcessing, setIsProcessing] = useState(false);
+  function uploadFiles(event: any) {
     const fileList = event.target.files;
 
     if (!fileList || fileList.length === 0) {
       console.error("No files selected.");
       return;
-  }
-  
-  for (let i = 0; i < fileList.length; i++) {
-    if (!fileList[i].type.startsWith('image/png')) {
+    }
+    setIsProcessing(true); //preventing duplicate uploads due to trigger based on file updates on Canvas component
+    for (let i = 0; i < fileList.length; i++) {
+      if (!fileList[i].type.startsWith("image/png")) {
         console.error("Only PNG files are allowed.");
         continue; //skips to next image to see if it passes the criteria
+      } else {
+        //we need a dataURL for the image for compatibility with ImageTracer
+        const fileName = fileList[i].name.substring(
+          0,
+          fileList[i].name.lastIndexOf(".")
+        );
+        const icon = fileList[i];
+        const reader = new FileReader();
+       
+        reader.onload = (e) => {
+          const dataUrl = e.target?.result as string;
+          setFileData((prevFiles) => {
+            const updatedFiles = [...prevFiles, { name: fileName, data: dataUrl }];
+            if (updatedFiles.length === fileList.length) {
+              setIsProcessing(false);
+            }
+            return updatedFiles;
+          });
+        };
+
+        reader.readAsDataURL(icon);
+      }
     }
-
-    else{ //we need a dataURL for the image for compatibility with ImageTracer
-      const fileName = fileList[i].name.substring(0, fileList[i].name.lastIndexOf("."));
-      const icon = fileList[i];
-      const reader = new FileReader(); 
-      reader.onload = (e) => {
-        const dataUrl = e.target?.result as string;// Log the data URL
-        setFileData(prevFiles => [...prevFiles, {name: fileName, data: dataUrl}]); //pulls previous fileData and adds new entry //I NEED TO KNOW WHY prevFiles worked?
-        console.log("File uploaded:", fileName, i);
-
-        setTimeout(() => {
-          console.log("Updated File array:", fileData);
-        }, 0);
-      };
-
-
-      reader.readAsDataURL(icon);
-
-      
-    }
-  
   }
-  
-  
-  }
-/*
+  /*
     useEffect(() => {
       if (canvasEl.current) {
         createCanvas(canvasEl.current);
       }
     }, []);
 */
- 
 
-    return (
-        <>
-        
-        <div className="border-[10px] border-dashed h-[250px] w-[250px] justify-content content-center">
-        <p>
-          Drag and Drop
-        </p>
+  return (
+    <>
+      <div className="border-[10px] border-dashed h-[250px] w-[250px] justify-content content-center">
+        <p>Drag and Drop</p>
         <p>or</p>
-        <button onClick={() => fileInputRef.current?.click()} className='bg-blue-500 px-[5px] mt-[10px]'>
-            Upload Files
+        <button
+          onClick={() => fileInputRef.current?.click()}
+          className="bg-blue-500 px-[5px] mt-[10px]"
+        >
+          Upload Files
         </button>
 
-
         <input
-              type="file"
-              id="files"
-              name="files"
-              multiple
-              accept="image/png"
-              ref={fileInputRef}
-              className="hidden"
-              onChange={uploadFiles}
-            />
-
-
- 
+          type="file"
+          id="files"
+          name="files"
+          multiple
+          accept="image/png"
+          ref={fileInputRef}
+          className="hidden"
+          onChange={uploadFiles}
+        />
       </div>
-        
-        <MainCanvas /*imgUpload={uploadedFile} imgName={uploadedName}*/ files={fileData}/>
 
-       
-        </>
-
-    );
+      <MainCanvas
+        /*imgUpload={uploadedFile} imgName={uploadedName}*/ files={isProcessing ? [] : fileData}
+      />
+    </>
+  );
 }
-
